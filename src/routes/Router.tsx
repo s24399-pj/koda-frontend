07e07ec278c29bbc6166d97f86e36fdfce33c24b
s25670-pj <92204700+s25670-pj@users.noreply.github.com>
2025-03-22 13:23:53 +0100

    Add offer page (#8)
    * added simple home page
    
    * added whychooseus component
    
    * added most viewed cars
    
    * css to scss
    
    * added offer page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home/HomePage";
+import Offer from "../pages/Offer/Offer";
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/offer/:id" element={<Offer />} />
             </Routes>
         </Router>
     );
 };
-
+       
 export default AppRouter;
