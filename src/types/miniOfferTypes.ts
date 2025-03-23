95fbd93ce86867a8020da4644b7f82fa7fc4a9ec
s25670-pj <s25670@pjwstk.edu.pl>
2025-03-23 15:34:03 +0100

    added offer slider

---
+++ src/types/miniOfferTypes.ts
@@ -0,0 +1,11 @@
+export interface MiniOffer {
+    id: string;
+    title: string;
+    price: number;
+    mainImage: string;
+    mileage: number;
+    fuelType: string;
+    year: number;
+    enginePower: number;
+    displacement: string;
+  }
