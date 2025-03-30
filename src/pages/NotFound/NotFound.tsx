37c480ccdefffd2d86e67d7acbcba615b86f2e8d
s25670-pj <s25670@pjwstk.edu.pl>
2025-03-30 20:58:08 +0200

    added not found page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
+import carNotFound from "../../assets/images/car_not_found.png";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="not-found-container">
+      <img src={carNotFound} alt="Car Not Found" className="not-found-image" />
+      <h1>404 - Strona nie znaleziona</h1>
+      <p>Ups! Wygląda na to, że zabłądziłeś. Wracamy do głównej?</p>
+      <Link to="/" className="back-home-button">Powrót do strony głównej</Link>
+    </div>
+  );
+};
+
+export default NotFound;
