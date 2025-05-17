b9a5689d7dd45f9d8483482f48379127f863f45e
alopalka <alopalka@proton.me>
2025-05-17 16:57:54 +0200

    semi working changes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+window.global = window;
+window.process = {
+    env: { DEBUG: undefined },
+};
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import HomePage from "../pages/Home/HomePage";
 import Offer from "../pages/Offer/Offer";
 import LoginPage from "../pages/Auth/LoginPage.tsx";
@@ -9,6 +9,7 @@ import UserPanel from "../pages/UserPanel/UserPanel.tsx";
 import OfferComparison from "../pages/OfferComparison/OfferComparison.tsx";
 import WhyUs from "../pages/WhyUs/WhyUs.tsx";
 import OfferCreation from "../pages/OfferCreation/OfferCreation.tsx";
+import ChatPage from "../pages/Chat/ChatPage.tsx";
 import LikedOffers from "../pages/LikedOffers/LikedOffers.tsx";
 import RequireAuth from "../components/RequireAuth/RequireAuth.tsx";
 
@@ -16,27 +17,30 @@ const AppRouter = () => {
     return (
         <Routes>
             {/* Public */}
-            <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/offers" element={<OfferList />} />
-            <Route path="/offer/:id" element={<Offer />} />
-            <Route path="comparison" element={<OfferComparison />} />
-            <Route path="/whyus" element={<WhyUs />} />
-            <Route path="/user/login" element={<LoginPage />} />
-            <Route path="/user/register" element={<RegisterPage />} />
-            <Route path="/liked" element={<LikedOffers />} />
-           
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="*" element={<NotFound/>}/>
+            <Route path="/offers" element={<OfferList/>}/>
+            <Route path="/offer/:id" element={<Offer/>}/>
+            <Route path="comparison" element={<OfferComparison/>}/>
+            <Route path="/whyus" element={<WhyUs/>}/>
+            <Route path="/user/login" element={<LoginPage/>}/>
+            <Route path="/user/register" element={<RegisterPage/>}/>
+            <Route path="/liked" element={<LikedOffers/>}/>
+
             {/* Protected */}
             <Route path="/user/panel" element={
                 <RequireAuth>
-                    <UserPanel />
+                    <UserPanel/>
                 </RequireAuth>
-            } />
+            }/>
             <Route path="/offer/create" element={
                 <RequireAuth>
-                    <OfferCreation />
+                    <OfferCreation/>
                 </RequireAuth>
-            } />
+            }/>
+
+            <Route path="/chat" element={<ChatPage/>}/>
+            <Route path="/chat/:recipientId" element={<ChatPage/>}/>
         </Routes>
     );
 };
