62a30c3053ed3b241632336daee51daf65c1f2f5
s25323-pj <s25323@pjwstk.edu.pl>
2025-05-31 19:21:48 +0200

    hardocde to normal

--- src/api/imageApi.ts
+++ src/api/imageApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8137/api/v1';
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
 
 export interface ImageUploadResponse {
     id: string;
