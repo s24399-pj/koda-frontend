53580be03657f928bfa0675868d9879af9ace32d
s25323-pj <s25323@pjwstk.edu.pl>
2025-06-09 20:40:57 +0200

    hardocde to normal

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -11,42 +11,44 @@ import WhyUs from '../pages/WhyUs/WhyUs.tsx';
 import OfferCreation from '../pages/OfferCreation/OfferCreation.tsx';
 import ChatPage from '../pages/Chat/ChatPage.tsx';
 import LikedOffers from '../pages/LikedOffers/LikedOffers.tsx';
+import SellerOffers from '../pages/SellerOffers/SellerOffers.tsx';
 import RequireAuth from '../components/RequireAuth/RequireAuth.tsx';
 
 const AppRouter = () => {
-  return (
-    <Routes>
-      {/* Public */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="*" element={<NotFound />} />
-      <Route path="/offers" element={<OfferList />} />
-      <Route path="/offer/:id" element={<Offer />} />
-      <Route path="comparison" element={<OfferComparison />} />
-      <Route path="/whyus" element={<WhyUs />} />
-      <Route path="/user/login" element={<LoginPage />} />
-      <Route path="/user/register" element={<RegisterPage />} />
-      <Route path="/liked" element={<LikedOffers />} />
-      {/* Protected */}
-      <Route
-        path="/user/panel"
-        element={
-          <RequireAuth>
-            <UserPanel />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path="/offer/create"
-        element={
-          <RequireAuth>
-            <OfferCreation />
-          </RequireAuth>
-        }
-      />
-      <Route path="/chat" element={<ChatPage />} />
-      <Route path="/chat/:recipientId" element={<ChatPage />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            {/* Public */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/offers" element={<OfferList />} />
+            <Route path="/offer/:id" element={<Offer />} />
+            <Route path="/seller/:sellerId/offers" element={<SellerOffers />} />
+            <Route path="comparison" element={<OfferComparison />} />
+            <Route path="/whyus" element={<WhyUs />} />
+            <Route path="/user/login" element={<LoginPage />} />
+            <Route path="/user/register" element={<RegisterPage />} />
+            <Route path="/liked" element={<LikedOffers />} />
+            {/* Protected */}
+            <Route
+                path="/user/panel"
+                element={
+                    <RequireAuth>
+                        <UserPanel />
+                    </RequireAuth>
+                }
+            />
+            <Route
+                path="/offer/create"
+                element={
+                    <RequireAuth>
+                        <OfferCreation />
+                    </RequireAuth>
+                }
+            />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/chat/:recipientId" element={<ChatPage />} />
+        </Routes>
+    );
 };
 
 export default AppRouter;
