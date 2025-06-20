6886e80ab42dd1a498b9e73e8f109a5d673d84f4
s25670-pj <s25670@pjwstk.edu.pl>
2025-06-20 13:30:17 +0200

    added server config for local

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
   plugins: [react()],
   resolve: {
     alias: {
